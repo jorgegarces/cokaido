73df7f07c889557cf7bf8d26446c06413e872506
Jorge Garcés <jgarces@admins-MacBook-Pro.local>
2019-11-22 17:05:08 +0100

    refactor: recursively convert 7 8 to roman numbers

--- romansTPP/src/main/java/RomanTPP.java
+++ romansTPP/src/main/java/RomanTPP.java
@@ -7,14 +7,13 @@ public class RomanTPP {
 
         if (arabicNumber < 4) return "I" + convert(arabicNumber - 1);
 
+        if (arabicNumber == 5) return "V";
+
+        if (arabicNumber < 9) return "V" + convert(arabicNumber - 5);
+
         HashMap<Integer, String> dictionaryOfRomanNumbers = new HashMap<Integer, String>();
 
         dictionaryOfRomanNumbers.put(4, "IV");
-        dictionaryOfRomanNumbers.put(5, "V");
-        dictionaryOfRomanNumbers.put(6, "VI");
-        dictionaryOfRomanNumbers.put(7, "VII");
-        dictionaryOfRomanNumbers.put(8, "VIII");
-
 
         return dictionaryOfRomanNumbers.get(arabicNumber);
     }
