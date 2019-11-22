c5ddba21a089972ae18822d1a342ed8aa922a09e
Jorge Garcés <jgarces@admins-MacBook-Pro.local>
2019-11-22 16:29:44 +0100

    refactor: recursively convert 1 2 3 to roman numbers

--- romansTPP/src/main/java/RomanTPP.java
+++ romansTPP/src/main/java/RomanTPP.java
@@ -2,13 +2,17 @@ import java.util.HashMap;
 
 public class RomanTPP {
     public String convert(int arabicNumber) {
+
+        if (arabicNumber == 1) return "I";
+
+        if (arabicNumber < 4) return "I" + convert(arabicNumber - 1);
+
         HashMap<Integer, String> dictionaryOfRomanNumbers = new HashMap<Integer, String>();
-        dictionaryOfRomanNumbers.put(1, "I");
-        dictionaryOfRomanNumbers.put(2, "II");
-        dictionaryOfRomanNumbers.put(3, "III");
+
         dictionaryOfRomanNumbers.put(4, "IV");
         dictionaryOfRomanNumbers.put(5, "V");
         dictionaryOfRomanNumbers.put(6, "VI");
+
         return dictionaryOfRomanNumbers.get(arabicNumber);
     }
 }
