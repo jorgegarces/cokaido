e5c4ae876a74c237d2f239fde6dbeb0be5bfb704
Jorge Garcés <jgarces@admins-MacBook-Pro.local>
2019-11-22 15:58:07 +0100

    refactor: romanTPP to work with roman numbers into a dictionary from 1 to 6

--- romansTPP/src/main/java/RomanTPP.java
+++ romansTPP/src/main/java/RomanTPP.java
@@ -1,6 +1,14 @@
+import java.util.HashMap;
+
 public class RomanTPP {
     public String convert(int arabicNumber) {
-        String[] romanNumbers = {"I","II","III","IV","V","VI"};
-        return romanNumbers[arabicNumber - 1];
+        HashMap<Integer, String> dictionaryOfRomanNumbers = new HashMap<Integer, String>();
+        dictionaryOfRomanNumbers.put(1, "I");
+        dictionaryOfRomanNumbers.put(2, "II");
+        dictionaryOfRomanNumbers.put(3, "III");
+        dictionaryOfRomanNumbers.put(4, "IV");
+        dictionaryOfRomanNumbers.put(5, "V");
+        dictionaryOfRomanNumbers.put(6, "VI");
+        return dictionaryOfRomanNumbers.get(arabicNumber);
     }
 }
