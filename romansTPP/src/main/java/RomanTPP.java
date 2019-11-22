c1e559720926ec3c26521efcaaaacbc8c1eda409
Jorge Garcés <jgarces@admins-MacBook-Pro.local>
2019-11-22 15:46:09 +0100

    refactor: romanTPP to work with roman numbers into an array from 1 to 6

--- romansTPP/src/main/java/RomanTPP.java
+++ romansTPP/src/main/java/RomanTPP.java
@@ -1,8 +1,6 @@
 public class RomanTPP {
     public String convert(int arabicNumber) {
-        if (arabicNumber == 4) return "IV";
-        if (arabicNumber == 5) return "V";
-        if (arabicNumber == 6) return "VI";
-        return new String(new char[arabicNumber]).replace("\0", "I");
+        String[] romanNumbers = {"I","II","III","IV","V","VI"};
+        return romanNumbers[arabicNumber - 1];
     }
 }
