6271a6a28ad6ef79b1be654328dd0dbc73bcef8e
Jorge Garces <jorge.garces@lifullconnect.com>
2019-11-27 14:35:00 +0100

    no hay tiempo para más

--- romansTPP/src/test/java/RomanTPPShould.java
+++ romansTPP/src/test/java/RomanTPPShould.java
@@ -15,7 +15,14 @@ public class RomanTPPShould {
             "5,V",
             "6,VI",
             "7,VII",
-            "8,VIII"
+            "8,VIII",
+            "9,IX",
+            "10,X",
+            "11,XI",
+            "33,XXXIII",
+            "40,XL",
+            "50,L",
+            "52,LII"
     })
     public void return_roman_number_when_arabic_number_is_passed(int arabicNumber, String romanNumber){
 
