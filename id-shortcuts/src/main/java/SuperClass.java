78a133d83ae9bf6d1b5ee419711d923652a963e9
Jorge Garces <jorge.garces@lifullconnect.com>
2019-12-17 09:40:42 +0100

    progressing

---
+++ id-shortcuts/src/main/java/SuperClass.java
@@ -0,0 +1,8 @@
+public abstract class SuperClass {
+    public static final int CONSTANT = 0;
+    public int varInt;
+
+    public abstract void setVarDouble(double var);
+
+    public abstract double getVarDouble();
+}
