222d0fa4170450c8356432e10821237d7fd61b59
Jorge Garcés <jgarcesembarba@gmail.com>
2020-01-10 17:06:49 +0100

    test: ensure_one_user_is_not_friends_with_another

--- src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -13,4 +13,11 @@ public class UserTest {
         assertTrue(user.isFriendsWith(friend));
     }
 
+    @Test
+    public void ensure_one_user_is_not_friends_with_another(){
+        User user = new User();
+        User friend = new User();
+
+        assertFalse(user.isFriendsWith(friend));
+    }
 }
