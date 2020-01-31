c2b865d900df78ae9f74ac7d20bca4d86f3a3115
Jorge Garcés <jgarcesembarba@gmail.com>
2020-01-31 14:57:05 +0100

    refactor: LongMethods done

--- src/main/java/Bloaters/Longmethods/TechnicalDebt.java
+++ src/main/java/Bloaters/Longmethods/TechnicalDebt.java
@@ -7,49 +7,44 @@ import java.util.List;
 
 public class TechnicalDebt {
     private List<Issue> issues = new ArrayList<Issue>();
-
     private float total;
-
-    private Issue lastIssue;
-
     private String lastIssueDate;
 
     public void fixed(float amount)
     {
-        setTotal(getTotal() - amount);
+        this.total = total - amount;
     }
 
     public void register(float effortManHours, String description)
     {
-        if (effortManHours > 1000 || effortManHours <= 0)
-        {
-            throw new RuntimeException("Cannot register tech debt where effort is bigger than 1000 man hours to fix");
-        }
+        Priority priority = checkPriority(effortManHours);
 
-        Priority priority = Priority.Low;
+        this.total = total + effortManHours;
 
-        if (effortManHours > 100)
-        {
-            priority = Priority.Medium;
-        }
+        issues.add(new Issue(priority));
 
-        if (effortManHours > 250)
-        {
-            priority = Priority.High;
-        }
+        updateLastIssue();
+    }
 
-        if (effortManHours > 500)
-        {
-            priority = Priority.Critical;
-        }
-
-        setTotal(getTotal() + effortManHours);
-
-        issues.add(new Issue(effortManHours, description, priority));
-
+    private void updateLastIssue() {
         Calendar now = Calendar.getInstance();
         now.setTime(new Date());
-        setLastIssueDate(now.get(Calendar.DAY_OF_MONTH) + "/" + now.get(Calendar.MONTH) + "/" + now.get(Calendar.YEAR));
+        this.lastIssueDate = now.get(Calendar.DAY_OF_MONTH) + "/" + now.get(Calendar.MONTH) + "/" + now.get(Calendar.YEAR);
+    }
+
+    private Priority checkPriority(float effortManHours) {
+        checkIfOffBounds(effortManHours);
+
+        if (effortManHours > 100) return Priority.Medium;
+        if (effortManHours > 250) return Priority.High;
+        if (effortManHours > 500) return Priority.Critical;
+
+        return Priority.Low;
+    }
+
+    private void checkIfOffBounds(float effortManHours) {
+        if (effortManHours > 1000 || effortManHours <= 0)
+            throw new RuntimeException("Cannot register tech debt where effort is bigger than 1000 man hours to fix");
     }
 
     public Issue getLastIssue() {
@@ -60,15 +55,7 @@ public class TechnicalDebt {
         return lastIssueDate;
     }
 
-    public void setLastIssueDate(String lastIssueDate) {
-        this.lastIssueDate = lastIssueDate;
-    }
-
     public float getTotal() {
         return total;
-    }
-
-    public void setTotal(float total) {
-        this.total = total;
     }
 }
