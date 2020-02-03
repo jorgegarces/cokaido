f570caa25fd4b89d33f3fa30b05d21096548edb5
Jorge Garcés <jgarcesembarba@gmail.com>
2020-02-03 11:18:50 +0100

    refactoring code smells on Board winner method

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,80 +1,61 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Board
-{
+public class Board {
     private List<Tile> _plays = new ArrayList<>();
 
-    public Board()
-    {
-        for (int i = 0; i < 3; i++) // One level of indentation
-        {
+    public Board() {
+        generateBoard();
+    }
+
+    private void generateBoard() {
+        for (int i = 0; i < 3; i++) {
             createVerticalPositions(i);
         }
     }
 
     private void createVerticalPositions(int i) {
-        for (int j = 0; j < 3; j++)
-        {
-            Tile tile = new Tile(i, j,  ' ');
+        for (int j = 0; j < 3; j++) {
+            Tile tile = new Tile(i, j, ' ');
             _plays.add(tile);
         }
     }
 
-    public Tile tileAt(int x, int y)
-    {
+    public Tile tileAt(int x, int y) {
         for (Tile tile : _plays) {
-            if (tile.hasSamePosition(x, y)){
+            if (tile.hasSamePosition(x, y)) {
                 return tile;
             }
         }
         return null;
     }
 
-    public void AddTileAt(char symbol, int x, int y)
-    {
-        tileAt(x,y).Symbol = symbol;
+    public void AddTileAt(char symbol, int x, int y) {
+        tileAt(x, y).Symbol = symbol;
     }
 
-    public char winner() {  // long method // duplication
-        //if the positions in first row are taken  // no coments
-        if (tileAt(0, 0).Symbol != ' ' &&
-                tileAt(0, 1).Symbol != ' ' &&
-                tileAt(0, 2).Symbol != ' ') {
-            //if first row is full with same symbol
-            if (tileAt(0, 0).Symbol ==
-                    tileAt(0, 1).Symbol &&
-                    tileAt(0, 2).Symbol == tileAt(0, 1).Symbol) {
-                return tileAt(0, 0).Symbol;
+    public char winner() {  // long method
+        for (int i = 0; i < 3; i++) {
+            if (rowIsNotEmpty(i)) {
+                if (isWinningRow(i)) {
+                    return tileAt(i, 0).Symbol;
+                }
             }
-        }
 
-        //if the positions in first row are taken
-        if (tileAt(1, 0).Symbol != ' ' &&
-                tileAt(1, 1).Symbol != ' ' &&
-                tileAt(1, 2).Symbol != ' ') {
-            //if middle row is full with same symbol
-            if (tileAt(1, 0).Symbol ==
-                    tileAt(1, 1).Symbol &&
-                    tileAt(1, 2).Symbol ==
-                            tileAt(1, 1).Symbol) {
-                return tileAt(1, 0).Symbol;
-            }
-        }
-
-        //if the positions in first row are taken
-        if (tileAt(2, 0).Symbol != ' ' &&
-                tileAt(2, 1).Symbol != ' ' &&
-                tileAt(2, 2).Symbol != ' ') {
-            //if middle row is full with same symbol
-            if (tileAt(2, 0).Symbol ==
-                    tileAt(2, 1).Symbol &&
-                    tileAt(2, 2).Symbol ==
-                            tileAt(2, 1).Symbol) {
-                return tileAt(2, 0).Symbol;
-            }
         }
 
         return ' ';
+    }
+
+    private boolean isWinningRow(int i) {
+        return tileAt(i, 0).Symbol ==
+                tileAt(i, 1).Symbol &&
+                tileAt(i, 2).Symbol == tileAt(i, 1).Symbol;
+    }
+
+    private boolean rowIsNotEmpty(int i) {
+        return tileAt(i, 0).Symbol != ' ' &&
+                tileAt(i, 1).Symbol != ' ' &&
+                tileAt(i, 2).Symbol != ' ';
     }
 }
