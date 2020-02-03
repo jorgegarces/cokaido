4ed15b978266866fafead364332d7ac4efad04f8
Jorge Garcés <jgarcesembarba@gmail.com>
2020-02-03 10:42:58 +0100

    refactoring code smells move winer method to Board

---
+++ src/main/java/Board.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board
+{
+    private List<Tile> _plays = new ArrayList<>();
+
+    public Board()
+    {
+        for (int i = 0; i < 3; i++) // One level of indentation
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Tile tile = new Tile(); // Inappropiate intimacy
+                tile.X = i;
+                tile.Y = j;
+                tile.Symbol = ' ';
+                _plays.add(tile);
+            }
+        }
+    }
+
+    public Tile TileAt(int x, int y)
+    {
+        for (Tile t : _plays) {
+            if (t.X == x && t.Y == y){
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public void AddTileAt(char symbol, int x, int y)
+    {
+        Tile newTile = new Tile();
+        newTile.X = x;
+        newTile.Y = y;
+        newTile.Symbol = symbol;
+
+        TileAt(x,y).Symbol = symbol;
+    }
+
+    public char Winner() {  // long method // duplication
+        //if the positions in first row are taken  // no coments
+        if (TileAt(0, 0).Symbol != ' ' &&  // Message chain
+                TileAt(0, 1).Symbol != ' ' &&
+                TileAt(0, 2).Symbol != ' ') {
+            //if first row is full with same symbol
+            if (TileAt(0, 0).Symbol ==
+                    TileAt(0, 1).Symbol &&
+                    TileAt(0, 2).Symbol == TileAt(0, 1).Symbol) {
+                return TileAt(0, 0).Symbol;
+            }
+        }
+
+        //if the positions in first row are taken
+        if (TileAt(1, 0).Symbol != ' ' &&
+                TileAt(1, 1).Symbol != ' ' &&
+                TileAt(1, 2).Symbol != ' ') {
+            //if middle row is full with same symbol
+            if (TileAt(1, 0).Symbol ==
+                    TileAt(1, 1).Symbol &&
+                    TileAt(1, 2).Symbol ==
+                            TileAt(1, 1).Symbol) {
+                return TileAt(1, 0).Symbol;
+            }
+        }
+
+        //if the positions in first row are taken
+        if (TileAt(2, 0).Symbol != ' ' &&
+                TileAt(2, 1).Symbol != ' ' &&
+                TileAt(2, 2).Symbol != ' ') {
+            //if middle row is full with same symbol
+            if (TileAt(2, 0).Symbol ==
+                    TileAt(2, 1).Symbol &&
+                    TileAt(2, 2).Symbol ==
+                            TileAt(2, 1).Symbol) {
+                return TileAt(2, 0).Symbol;
+            }
+        }
+
+        return ' ';
+    }
+}
---
+++ src/main/java/Game.java
@@ -0,0 +1,28 @@
+public class Game {
+    private char _lastSymbol = ' ';
+    Board _board = new Board();
+
+    public void Play(char symbol, int x, int y) throws Exception { //primitive obsesion
+        //if first move
+        if (_lastSymbol == ' ') {
+            //if player is X
+            if (symbol == 'O') {
+                throw new Exception("Invalid first player");
+            }
+        }
+        //if not first move but player repeated
+        else if (symbol == _lastSymbol) {
+            throw new Exception("Invalid next player");
+        }
+        //if not first move but play on an already played tile
+        else if (_board.TileAt(x, y).Symbol != ' ') { // mesage chain
+            throw new Exception("Invalid position");
+        }
+
+        // update game state
+        _lastSymbol = symbol;
+        _board.AddTileAt(symbol, x, y);
+    }
+
+}
+
