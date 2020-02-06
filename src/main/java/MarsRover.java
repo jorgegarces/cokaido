17d18515d9b50182563c6a67073e95453c7c0532
Jorge Garcés <jgarcesembarba@gmail.com>
2020-02-06 16:57:04 +0100

    refactor: move navigation methods to Position class

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -19,39 +19,12 @@ public class MarsRover {
     }
 
     private void run(char command) {
-        if (command == 'F') moveForward();
-        if (command == 'R') rotateRight();
-        if (command == 'L') rotateLeft();
-        if (command == 'B') moveBackwards();
+        if (command == 'F') position.moveForward();
+        if (command == 'R') position.rotateRight();
+        if (command == 'L') position.rotateLeft();
+        if (command == 'B') position.moveBackwards();
     }
 
-    private void rotateLeft() {
-        if (this.position.orientation == 'N') this.position.orientation = 'W';
-        else if (this.position.orientation == 'E') this.position.orientation = 'N';
-        else if (this.position.orientation == 'S') this.position.orientation = 'E';
-        else if (this.position.orientation == 'W') this.position.orientation = 'S';
-    }
-
-    private void rotateRight() {
-        if (this.position.orientation == 'N') this.position.orientation = 'E';
-        else if (this.position.orientation == 'E') this.position.orientation = 'S';
-        else if (this.position.orientation == 'S') this.position.orientation = 'W';
-        else if (this.position.orientation == 'W') this.position.orientation = 'N';
-    }
-
-    private void moveForward() {
-        if (this.position.orientation == 'N') this.position.positionY = this.position.positionY + 1;
-        if (this.position.orientation == 'E') this.position.positionX = this.position.positionX + 1;
-        if (this.position.orientation == 'S') this.position.positionY = this.position.positionY - 1;
-        if (this.position.orientation == 'W') this.position.positionX = this.position.positionX - 1;
-    }
-
-    private void moveBackwards() {
-        if (this.position.orientation == 'N') this.position.positionY = this.position.positionY - 1;
-        if (this.position.orientation == 'E') this.position.positionX = this.position.positionX - 1;
-        if (this.position.orientation == 'S') this.position.positionY = this.position.positionY + 1;
-        if (this.position.orientation == 'W') this.position.positionX = this.position.positionX + 1;
-    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -10,4 +10,32 @@ public class Position {
         this.positionY = positionY;
         this.orientation = orientation;
     }
+
+    void rotateLeft() {
+        if (orientation == 'N') orientation = 'W';
+        else if (orientation == 'E') orientation = 'N';
+        else if (orientation == 'S') orientation = 'E';
+        else if (orientation == 'W') orientation = 'S';
+    }
+
+    void rotateRight() {
+        if (orientation == 'N') orientation = 'E';
+        else if (orientation == 'E') orientation = 'S';
+        else if (orientation == 'S') orientation = 'W';
+        else if (orientation == 'W') orientation = 'N';
+    }
+
+    void moveForward() {
+        if (orientation == 'N') positionY = positionY + 1;
+        if (orientation == 'E') positionX = positionX + 1;
+        if (orientation == 'S') positionY = positionY - 1;
+        if (orientation == 'W') positionX = positionX - 1;
+    }
+
+    void moveBackwards() {
+        if (orientation == 'N') positionY = positionY - 1;
+        if (orientation == 'E') positionX = positionX - 1;
+        if (orientation == 'S') positionY = positionY + 1;
+        if (orientation == 'W') positionX = positionX + 1;
+    }
 }
