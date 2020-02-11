83925ddd423b527e79872a86f099358735a97239
Felipe Fernández <felipe.fernandez@lifullconnect.com>
2020-02-11 17:08:30 +0100

    coffee machine decorator pattern

--- coffe-machine/carol-felipe/src/main/java/IngredientsDecorator.java
+++ coffe-machine/carol-felipe/src/main/java/IngredientsDecorator.java
@@ -1,10 +1,4 @@
 public abstract class IngredientsDecorator implements Coffee {
 
-    public double price() {
-        return 0;
-    }
 
-    public String name() {
-        return null;
-    }
 }
