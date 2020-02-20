b64c75f74b4b2786372569683b66608efcaa8bf8
Jorge Garcés <jgarcesembarba@gmail.com>
2020-02-20 17:35:20 +0100

    feat: return statement with a deposit of 1000

--- bankata-outside-in/src/main/java/app/repositories/TransactionRepository.java
+++ bankata-outside-in/src/main/java/app/repositories/TransactionRepository.java
@@ -1,12 +1,26 @@
 package app.repositories;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TransactionRepository {
+    private List<Transaction> transactions;
 
-   public void save(Transaction transaction){
-   }
+    public TransactionRepository() {
+        this.transactions = new ArrayList<>();
+    }
 
-   public String getStatement() {
-      return "date       || credit   || debit    || balance\n";
-   }
+    public void save(Transaction transaction) {
+
+        this.transactions.add(transaction);
+    }
+
+    public String getStatement() {
+        StringBuilder statement = new StringBuilder("date       || credit   || debit    || balance\n");
+
+        for (Transaction transaction : transactions) statement.append(transaction);
+
+        return statement.toString();
+    }
 
 }
--- bankata-outside-in/src/test/java/TransactionRepositoryShould.java
+++ bankata-outside-in/src/test/java/TransactionRepositoryShould.java
@@ -1,15 +1,37 @@
+import app.repositories.Deposit;
+import app.repositories.Transaction;
 import app.repositories.TransactionRepository;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class TransactionRepositoryShould {
+    @Mock
+    private Transaction transaction;
 
     @Test
-    public void return_empty_statement(){
+    public void return_empty_statement() {
         TransactionRepository transactionRepository = new TransactionRepository();
 
         Assert.assertEquals("date       || credit   || debit    || balance\n", transactionRepository.getStatement());
     }
 
+    @Test
+    public void return_statement_with_a_deposit_of_1000() {
+        MockitoAnnotations.initMocks(this);
+        TransactionRepository transactionRepository = new TransactionRepository();
+
+        when(transaction.toString()).thenReturn("10/01/2012 || 1000.00  ||          || 1000.00");
+        transactionRepository.save(transaction);
+
+        Assert.assertEquals("date       || credit   || debit    || balance\n10/01/2012 || 1000.00  ||          || 1000.00", transactionRepository.getStatement());
+    }
 
 }
