f5051f6861fba0927d40a0acc2d1168a120b48d3
Jorge Garcés <jgarcesembarba@gmail.com>
2020-02-25 15:02:26 +0100

    feat:

--- stock-portfolio/src/main/java/repository/OperationRepository.java
+++ stock-portfolio/src/main/java/repository/OperationRepository.java
@@ -1,7 +1,6 @@
 package repository;
 
 import app.AssetReport;
-import app.Portfolio;
 import app.PortfolioReport;
 import operation.Operation;
 
@@ -12,13 +11,12 @@ public class OperationRepository {
     private List<Operation> operations = new ArrayList<>();
 
     public void save(Operation operation) {
-    this.operations.add(operation);
+        this.operations.add(operation);
     }
 
     public PortfolioReport getPortfolioReport() {
         PortfolioReport portfolioReport = new PortfolioReport();
-        portfolioReport.assetsList.add(new AssetReport("Old School Waterfall Software LTD", "1000", "5,75", "2,875.00","sold 500 on 11/12/2018"));
+        portfolioReport.assetsList.add(new AssetReport("Old School Waterfall Software LTD", "1000", "5,75", "2,875.00", "sold 500 on 11/12/2018"));
         return portfolioReport;
     }
-
 }
