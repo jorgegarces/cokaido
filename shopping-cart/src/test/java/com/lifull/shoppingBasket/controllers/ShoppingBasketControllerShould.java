c31d232fae19b620b713609f6df15d342ae97487
Jorge Garcés <jgarcesembarba@gmail.com>
2020-03-13 09:53:47 +0100

    added classicist test for controller to throw an error if product quantity is negative

--- shopping-cart/src/test/java/com/lifull/shoppingBasket/controllers/ShoppingBasketControllerShould.java
+++ shopping-cart/src/test/java/com/lifull/shoppingBasket/controllers/ShoppingBasketControllerShould.java
@@ -1,130 +1,88 @@
 package com.lifull.shoppingBasket.controllers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.lifull.shoppingBasket.domain.ShoppingBasketService;
-import com.lifull.shoppingBasket.domain.memento.*;
+import com.lifull.shoppingBasket.domain.memento.ShoppingBasketMemento;
 import com.lifull.shoppingBasket.domain.product.Product;
 import com.lifull.shoppingBasket.domain.product.ProductId;
-import com.lifull.shoppingBasket.domain.shoppingBasket.ShoppingBasket;
 import com.lifull.shoppingBasket.domain.user.UserId;
 import com.lifull.shoppingBasket.infrastructure.InMemoryBasketRepository;
 import com.lifull.shoppingBasket.infrastructure.InMemoryProductRepository;
 import com.lifull.shoppingBasket.services.ITimeServer;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import java.util.Optional;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class ShoppingBasketControllerShould {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private InMemoryProductRepository productRepository;
-
-    @Autowired
-    private InMemoryBasketRepository basketRepository;
-
-    @Autowired
-    private ITimeServer timeServer;
-
-
+    @Mock
+    InMemoryBasketRepository basketRepository;
+    @Mock
+    InMemoryProductRepository productRepository;
+    @Mock
+    ITimeServer timeServer;
+    @InjectMocks
+    private ShoppingBasketController controller;
 
     @Test
-    public void add_new_item_to_basket_when_all_parameters_are_correct() throws Exception {
-        AddItemUseCase addItemUseCase = new AddItemUseCase();
-        addItemUseCase.productId = 20001;
-        addItemUseCase.userId = 1;
-        addItemUseCase.quantity = 1;
-        String jsonRequest = new ObjectMapper().writeValueAsString(addItemUseCase);
-        MvcResult result = this.mockMvc.perform(post("/shoppingBaskets").contentType(MediaType.APPLICATION_JSON)
-                .content(jsonRequest)).andDo(print()).andExpect(status().isCreated())
-                .andReturn();
+    public void retrieve_basket_from_userId() {
+        MockitoAnnotations.initMocks(this);
+        when(basketRepository.memento(new UserId(1))).thenReturn(new ShoppingBasketMemento());
 
-        String message = result.getResponse().getContentAsString();
+        ResponseEntity<ShoppingBasketMemento> basket = controller.shoppingBasketByUserId(1);
 
-        Assert.assertEquals(message, "Product added correctly");
+        Assert.assertNotNull(basket);
+        Assert.assertEquals(HttpStatus.OK, basket.getStatusCode());
     }
 
     @Test
-    public void throw_an_error_if_product_id_does_not_exist() throws Exception {
+    public void create_basket() {
+
+        MockitoAnnotations.initMocks(this);
+        when(productRepository.get(new ProductId(20))).thenReturn(java.util.Optional.of(new Product(new ProductId(20), "name", 1.00)));
         AddItemUseCase addItemUseCase = new AddItemUseCase();
-        addItemUseCase.productId = 20002;
         addItemUseCase.userId = 1;
+        addItemUseCase.productId = 20;
         addItemUseCase.quantity = 1;
-        String jsonRequest = new ObjectMapper().writeValueAsString(addItemUseCase);
-        MvcResult result = this.mockMvc.perform(post("/shoppingBaskets").contentType(MediaType.APPLICATION_JSON)
-                .content(jsonRequest)).andDo(print()).andExpect(status().isBadRequest())
-                .andReturn();
 
-        String message = result.getResponse().getContentAsString();
+        ResponseEntity<Object> responseExpected = controller.addItem(addItemUseCase);
 
-        Assert.assertEquals(message, "product does not exist");
+        Assert.assertEquals(HttpStatus.CREATED, responseExpected.getStatusCode());
+        Assert.assertEquals("Product added correctly", responseExpected.getBody());
     }
 
     @Test
-    public void throw_an_error_if_quantity_is_negative() throws Exception {
+    public void throw_an_error_if_product_id_does_not_exist() {
+
+        MockitoAnnotations.initMocks(this);
+        when(productRepository.get(new ProductId(2))).thenReturn(Optional.empty());
+
+        ResponseEntity<Object> response = controller.addItem(new AddItemUseCase());
+
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        Assert.assertEquals("product does not exist", response.getBody());
+    }
+
+    @Test
+    public void throw_an_error_if_product_quantity_is_negative() {
+
+        MockitoAnnotations.initMocks(this);
+        when(productRepository.get(new ProductId(20))).thenReturn(java.util.Optional.of(new Product(new ProductId(20), "name", 1.00)));
         AddItemUseCase addItemUseCase = new AddItemUseCase();
-        addItemUseCase.productId = 20001;
-        addItemUseCase.userId = 1;
+        addItemUseCase.productId = 20;
         addItemUseCase.quantity = -1;
-        String jsonRequest = new ObjectMapper().writeValueAsString(addItemUseCase);
-        MvcResult result = this.mockMvc.perform(post("/shoppingBaskets").contentType(MediaType.APPLICATION_JSON)
-                .content(jsonRequest)).andDo(print()).andExpect(status().isBadRequest())
-                .andReturn();
 
-        String message = result.getResponse().getContentAsString();
+        ResponseEntity<Object> response = controller.addItem(addItemUseCase);
 
-        Assert.assertEquals(message, "quantity cannot be negative");
-    }
-
-    @Test
-    public void retrieve_a_users_shopping_basket_memento() throws Exception {
-        ShoppingBasketMemento expectedMemento = new ShoppingBasketMemento();
-        expectedMemento.date = timeServer.getDate();
-        ProductMemento productMemento = new ProductMemento();
-        productMemento.name = "The Hobbit";
-        productMemento.price = 5.00;
-        LineItemMemento lineItemMemento = new LineItemMemento();
-        lineItemMemento.productMemento = productMemento;
-        lineItemMemento.quantity = 2;
-        lineItemMemento.total = 10.00;
-        expectedMemento.lineItemList = new LineItemListMemento();
-        expectedMemento.lineItemList.items.add(lineItemMemento);
-        expectedMemento.lineItemList.total = 10.00;
-        UserIdMemento userIdMemento = new UserIdMemento();
-        userIdMemento.id = 1;
-        expectedMemento.userId = userIdMemento;
-
-        ShoppingBasketService shoppingBasketService = new ShoppingBasketService(productRepository, basketRepository, timeServer);
-        shoppingBasketService.addItem(new UserId(1), new ProductId(10002), 2);
-
-        MvcResult result = this.mockMvc.perform(get("/shoppingBaskets?userId=1"))
-                .andDo(print()).andExpect(status().isOk())
-                .andReturn();
-
-        String contentAsString = result.getResponse().getContentAsString();
-
-        ShoppingBasketMemento memento = objectMapper.readValue(contentAsString, ShoppingBasketMemento.class);
-
-
-        Assert.assertEquals(expectedMemento, memento);
+        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        Assert.assertEquals("quantity cannot be negative", response.getBody());
     }
 }
-
